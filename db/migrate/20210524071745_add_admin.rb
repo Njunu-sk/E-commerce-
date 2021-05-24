7bff6f7ef326a6b503a8767b6d05aea86e20d476
Njunu-sk <simonnjunu16@gmail.com>
2021-05-24 10:27:28 +0300

    change user migration and add admin role

---
+++ db/migrate/20210524071745_add_admin.rb
@@ -0,0 +1,11 @@
+class AddAdmin < ActiveRecord::Migration[6.1]
+  def change
+    User.create! do |u|
+      u.email = 'admin@test.com'
+      u.password = 'password'
+      u.admin = true
+    end
+
+    # visit admin/dashboard
+  end
+end
