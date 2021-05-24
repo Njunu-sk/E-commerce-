ad0ab41e0279de87030e2f671eecc1d8725e155c
Njunu-sk <simonnjunu16@gmail.com>
2021-05-24 09:41:05 +0300

    finished domain logic and page stying

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,15 @@ Rails.application.routes.draw do
     resources :dashboard
   end
   root 'pages#index'
+
+  resources :categories, only: [:index] do
+    resources :products, only: [:index]
+  end
+
+  get '/cart', to: 'order_items#index'
+  resources :order_items, path: 'cart/items'
+
+  get '/cart/checkout', to: 'orders#new', as: :checkout
+  patch '/cart/checkout', to: 'orders#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
